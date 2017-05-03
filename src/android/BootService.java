95423fb5dc367f8692d7830f2c904ae47056f9fe
Vahn84 <vahn@Fabios-MacBook-Pro.local>
2017-05-03 14:33:57 +0200

    Cordova interface implemented

--- src/android/BootService.java
+++ src/android/BootService.java
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2015 Samsung Electronics Co., Ltd. All rights reserved. 
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that 
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
- * 
- *     * Redistributions of source code must retain the above copyright notice, 
- *       this list of conditions and the following disclaimer. 
- *     * Redistributions in binary form must reproduce the above copyright notice, 
- *       this list of conditions and the following disclaimer in the documentation and/or 
- *       other materials provided with the distribution. 
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation and/or
+ *       other materials provided with the distribution.
  *     * Neither the name of Samsung Electronics Co., Ltd. nor the names of its contributors may be used to endorse
  *       or promote products derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
@@ -23,16 +23,23 @@
 package com.vahn.cordova.onbootservice;
 
 import java.io.IOException;
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.nextadv.smartphoners.MainActivity;
 import com.samsung.android.sdk.SsdkUnsupportedException;
 import com.samsung.android.sdk.accessory.*;
+
+import org.apache.cordova.media.AudioPlayer;
 
 public class BootService extends SAAgent {
     private static final String TAG = "HelloAccessory(P)";
@@ -42,11 +49,12 @@ public class BootService extends SAAgent {
     private Context context;
     private static final String SHAREDPREFNAME = "PERCORSO";
     String filter = "com.vahn.cordova.onbootservice.BOOT_SERVICE_ACTION";
-
+    String message = "";
+    
     public BootService() {
         super(TAG, SASOCKET_CLASS);
     }
-
+    
     @Override
     public void onCreate() {
         super.onCreate();
@@ -65,29 +73,31 @@ public class BootService extends SAAgent {
              * without using this SDK, or you may want to notify user and close your application gracefully
              * (release resources, stop Service threads, close UI thread, etc.)
              */
-            stopSelf();
+            //stopSelf();
         }
     }
-
+    
+    
+    
     @Override
     public IBinder onBind(Intent intent) {
         return mBinder;
     }
-
+    
     @Override
     protected void onFindPeerAgentsResponse(SAPeerAgent[] peerAgents, int result) {
         Log.d(TAG, "onFindPeerAgentResponse : result =" + result);
     }
-
+    
     @Override
     protected void onServiceConnectionRequested(SAPeerAgent peerAgent) {
         Log.e(TAG, "onServiceConnectionResponse, CONNECTION REQUESTED");
         if (peerAgent != null) {
-
+            
             acceptServiceConnectionRequest(peerAgent);
         }
     }
-
+    
     @Override
     protected void onServiceConnectionResponse(SAPeerAgent peerAgent, SASocket socket, int result) {
         if (result == SAAgent.CONNECTION_SUCCESS) {
@@ -99,7 +109,7 @@ public class BootService extends SAAgent {
             Log.e(TAG, "onServiceConnectionResponse, CONNECTION_ALREADY_EXIST");
         }
     }
-
+    
     @Override
     protected void onAuthenticationResponse(SAPeerAgent peerAgent, SAAuthenticationToken authToken, int error) {
         /*
@@ -107,23 +117,23 @@ public class BootService extends SAAgent {
          * version of accessory device. Please refer to another sample application for Security.
          */
     }
-
+    
     @Override
     protected void onError(SAPeerAgent peerAgent, String errorMessage, int errorCode) {
         super.onError(peerAgent, errorMessage, errorCode);
     }
-
+    
     private boolean processUnsupportedException(SsdkUnsupportedException e) {
         e.printStackTrace();
         int errType = e.getType();
         if (errType == SsdkUnsupportedException.VENDOR_NOT_SUPPORTED
-                || errType == SsdkUnsupportedException.DEVICE_NOT_SUPPORTED) {
+            || errType == SsdkUnsupportedException.DEVICE_NOT_SUPPORTED) {
             /*
              * Your application can not use Samsung Accessory SDK. You application should work smoothly
              * without using this SDK, or you may want to notify user and close your app gracefully (release
              * resources, stop Service threads, close UI thread, etc.)
              */
-            stopSelf();
+            // stopSelf();
         } else if (errType == SsdkUnsupportedException.LIBRARY_NOT_INSTALLED) {
             Log.e(TAG, "You need to install Samsung Accessory SDK to use this application.");
         } else if (errType == SsdkUnsupportedException.LIBRARY_UPDATE_IS_REQUIRED) {
@@ -134,22 +144,23 @@ public class BootService extends SAAgent {
         }
         return true;
     }
-
+    
     public class LocalBinder extends Binder {
         public BootService getService() {
+            
             return BootService.this;
         }
     }
-
+    
     public class ServiceConnection extends SASocket {
+        
         public ServiceConnection() {
             super(ServiceConnection.class.getName());
         }
-
+        
         @Override
-        public void onError(int channelId, String errorMessage, int errorCode) {
-        }
-
+        public void onError(int channelId, String errorMessage, int errorCode) {}
+        
         @Override
         public void onReceive(int channelId, byte[] data) {
             if (mConnectionHandler == null) {
@@ -159,32 +170,38 @@ public class BootService extends SAAgent {
             /*
              Questa stringa 'action' andrà a prendersi il tipo di richiesta dalla variabile 'data'
              Attualmente viene settata di default come start e stop dei percorsi
-            */
+             */
             String action = "start_stop";
             
             context = getApplicationContext();
-            SharedPreferences sharedPref = context.getSharedPreferences(SHAREDPREFNAME, MODE_PRIVATE);
+            SharedPreferences sharedPref =  PreferenceManager.getDefaultSharedPreferences(context);
             SharedPreferences.Editor editor = sharedPref.edit();
-            boolean appIsRunning = sharedPref.getBoolean("appIsRunning", false);
+            
             
             if(action.equals("start_stop")){
-            
-            if(!appIsRunning) {
+                
                 editor.putBoolean("startedFromSW", true);
                 editor.commit();
-                Intent intent = new Intent(context, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(intent);
-            } else {
-                Intent intent = new Intent();
-                intent.setAction(filter);
-                context.sendBroadcast(intent);
-            }
+                
+                if(!sharedPref.getBoolean("appIsRunning", false)) {
+                    Intent intent = new Intent(context, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                } else {
+                    Intent intent = new Intent();
+                    intent.setAction(filter);
+                    context.sendBroadcast(intent);
+                }
+            } else if(action.equals("ask_for_data")){
                 
             }
-
-            final String message = sharedPref.getString("routeKms","0");
-
+            
+            try {
+                message  = sharedPref.getString("routeKms", "0.00");
+            } catch (Exception e) {
+                message  = Integer.toString(sharedPref.getInt("routeKms", 0));
+            }
+            
             new Thread(new Runnable() {
                 public void run() {
                     try {
@@ -195,11 +212,15 @@ public class BootService extends SAAgent {
                 }
             }).start();
         }
-
+        
         @Override
         protected void onServiceConnectionLost(int reason) {
             mConnectionHandler = null;
-
+            
         }
+        
+        
+        
     }
+    
 }
--- src/android/OnBootManagerPlugin.java
+++ src/android/OnBootManagerPlugin.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -18,7 +19,7 @@ import org.json.JSONObject;
  */
 
 public class OnBootManagerPlugin extends CordovaPlugin {
-
+    
     PluginResult result;
     Context context;
     CallbackContext cbContext;
@@ -29,41 +30,82 @@ public class OnBootManagerPlugin extends CordovaPlugin {
     private boolean routeIgnited;
     private String routeKms;
     private String routeMissdeCalls;
-
+    
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.cbContext = callbackContext;
         this.context = this.cordova.getActivity().getApplicationContext();
         this.context.registerReceiver(bootServiceActionReceiver, intentFilter);
-        this.sharedPref = context.getSharedPreferences(context.getPackageName() + "_preferences", Context.MODE_PRIVATE);
+        this.sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         this.editor = this.sharedPref.edit();
-
+        
+        if(action.equals("app_started")) {
+            
+            if(this.sharedPref.getBoolean("startedFromSW", false)) {
+                this.editor.putBoolean("startedFromSW",false);
+                this.editor.putBoolean("appIsRunning",true);
+                this.editor.commit();
+                sendActionToJS("route_start");
+            }
+            
+        } else if(action.equals("route_started")) {
+            
+            this.editor.putBoolean("routeStarted",true);
+            this.editor.commit();
+            
+        } else if(action.equals("route_stopped")){
+            
+            this.editor.putBoolean("routeStarted",false);
+            this.editor.commit();
+            
+        } else if(action.equals("route_trying_to_stop")){
+            sendActionToJS("stop_anyway");
+        }
+        
+        Log.d("calling_java", "executing");
         return true;
     }
-
+    
     public BroadcastReceiver bootServiceActionReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            sendActionToJS();
-        }
-    };
-
-
-    public void sendActionToJS(){
-
-            JSONObject parameter = new JSONObject();
-
-            try {
-                parameter.put("ignition", "start_stop");
-            } catch (JSONException e) {
-
-                Log.e("ERRORE SMARTPHONERS", e.toString());
-            }
-
-            result = new PluginResult(PluginResult.Status.OK, parameter);
-            result.setKeepCallback(true);
-            cbContext.sendPluginResult(result);
-
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    String action;
+    
+    if(sharedPref.getBoolean("routeStarted", false)) {
+        action = "route_stop";
+    } else {
+        action = "route_start";
     }
+    sendActionToJS(action);
+}
+};
 
+
+public void sendActionToJS(String action){
+Log.d("calling_js", "callback");
+JSONObject parameter = new JSONObject();
+
+try {
+parameter.put("ignition", action);
+} catch (JSONException e) {
+
+Log.e("ERRORE SMARTPHONERS", e.toString());
+}
+
+result = new PluginResult(PluginResult.Status.OK, parameter);
+result.setKeepCallback(true);
+cbContext.sendPluginResult(result);
+
+}
+
+@Override
+public void onDestroy() {
+Log.d("DESTROYING APP", "COMEON");
+this.editor.putBoolean("routeStarted",false);
+this.editor.putBoolean("startedFromSW",false);
+this.editor.putBoolean("appIsRunning",false);
+this.editor.putString("routeKms", "0.00");
+this.editor.commit();
+super.onDestroy();
+}
 }
